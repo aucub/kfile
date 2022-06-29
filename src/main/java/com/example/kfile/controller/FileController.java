ea8799c90cc508cb542eb684e9ff3b1fceeb5650
aucub <78630225+aucub@users.noreply.github.com>
2022-06-29 23:42:09 +0800

    update

--- src/main/java/com/example/kfile/controller/FileController.java
+++ src/main/java/com/example/kfile/controller/FileController.java
@@ -1,8 +1,11 @@
 package com.example.kfile.controller;
 
-import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
+import io.minio.*;
 import io.minio.errors.*;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,8 +15,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class FileController {
@@ -21,6 +27,47 @@ public class FileController {
     @GetMapping("/")
     public String uploadPage() {
         return "uploader";
+    }
+
+    MinioClient minioClient =
+            MinioClient.builder()
+                    .endpoint("http://127.0.0.1:9001")
+                    .credentials("sDRDIb2VaWKBRIn817J3", "AJMnqvSgWXNlCGwFp5lmYS3TqE76tDQv9itW1BFG")
+                    .build();
+
+    public String download(String filemd5, int expires, String customFilename) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        Map<String, String> reqParams = new HashMap<>();
+        reqParams.put("response-content-disposition", "attachment; filename=\"" + customFilename + "\"");
+        return minioClient.getPresignedObjectUrl(GetPresignedObjectUrlArgs.builder().bucket("file").object(filemd5).expiry(expires).extraQueryParams(reqParams).build());
+    }
+
+    public String delete(String filemd5) {
+        try {
+            minioClient.removeObject(RemoveObjectArgs.builder().bucket("file").object(filemd5).build());
+        } catch (ErrorResponseException | InsufficientDataException | InternalException | InvalidKeyException |
+                 InvalidResponseException | IOException | NoSuchAlgorithmException | ServerException |
+                 XmlParserException e) {
+            throw new RuntimeException(e);
+        }
+        return "删除成功";
+    }
+
+    @GetMapping("/download/filemd5")
+    public ResponseEntity<InputStreamResource> downloadFile(String filemd5, String filename) {
+        try {
+            InputStream stream = minioClient.getObject(GetObjectArgs.builder().bucket("file").object(filemd5).build());
+            InputStreamResource resource = new InputStreamResource(stream);
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.add("Content-Disposition", "attachment;filename=" + filename);
+            return ResponseEntity.ok()
+                    .headers(httpHeaders)
+                    .contentLength(resource.contentLength())
+                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                    .body(resource);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @PostMapping("/upload")
