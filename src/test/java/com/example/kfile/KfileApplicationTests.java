bc9677a522d51819e91a505d2e304093da26290f
aucub <78630225+aucub@users.noreply.github.com>
2022-07-01 00:00:00 +0800

    Initial commit

---
+++ src/test/java/com/example/kfile/KfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
